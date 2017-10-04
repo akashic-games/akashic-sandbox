fbb140378bd854ecc895597e2371a26f45c4e746
kamakiri01 <kamakiri01@users.noreply.github.com>
2017-10-04 23:21:44 +0900

    update build, dependencies

--- build/copyfiles.js
+++ build/copyfiles.js
@@ -15,11 +15,19 @@ function minify(filepath) {
 
 var files = [
 	"node_modules/@akashic/game-driver/build/game-driver.js",
-	"node_modules/@akashic/game-storage/build/game-storage.js",
 	"node_modules/@akashic/pdi-browser/build/pdi-browser.js"
 ];
 
 files.forEach(filepath => {
 	const outputPath = path.resolve("./js/v2", path.basename(filepath, ".js") + ".strip.js");
+	fs.writeFileSync(outputPath, minify(filepath).code);
+});
+
+var files_common = [
+	"node_modules/@akashic/game-storage/build/game-storage.js",
+];
+
+files_common.forEach(filepath => {
+	const outputPath = path.resolve("./js", path.basename(filepath, ".js") + ".strip.js");
 	fs.writeFileSync(outputPath, minify(filepath).code);
 });
