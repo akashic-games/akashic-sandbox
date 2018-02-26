7d095dbc020a69398e83bf5e1dead78347c0681b
kamakiri01 <kamakiri01@users.noreply.github.com>
2018-02-26 21:01:07 +0900

    fix revertSize

--- js/v1/developer.js
+++ js/v1/developer.js
@@ -944,35 +944,12 @@ function setupDeveloperMenu(param) {
 			},
 			toggleProfiler: toggleProfiler,
 			fitToWindow: function() {
-				var pf = props.driver._platform;
-				if (!pf.containerController) return;
-				var parentView = pf.containerView.parentElement;
-				parentView.style.margin = "0px";
-				parentView.style.padding = "0px";
-				parentView.style.overflow = "hidden";
-				var viewportSize = {
-					width: window.innerWidth || document.documentElement.clientWidth,
-					height: window.innerHeight || document.documentElement.clientHeight
-				};
-				var gameScale = Math.min(
-					viewportSize.width / pf.containerController._rendererReq.primarySurfaceWidth,
-					viewportSize.height / pf.containerController._rendererReq.primarySurfaceHeight
-				);
-				var gameSize = {
-					width: Math.floor(pf.containerController._rendererReq.primarySurfaceWidth * gameScale),
-					height: Math.floor(pf.containerController._rendererReq.primarySurfaceHeight * gameScale)
-				};
-				pf.containerController.changeScale(gameScale, gameScale);
-				// setOffsetしないとメニューにゲーム画面が被って、メニューサイズを変更しようとするとバグるのでとりあえず
-				var gameOffset = {
-					x: Math.floor((viewportSize.width - gameSize.width) / 2),
-					y: Math.floor((viewportSize.height - gameSize.height) / 2)
-				};
-				pf.containerController.inputHandlerLayer.setOffset(gameOffset);
+				// setOffsetしないとメニューにゲーム画面が被って、メニューサイズを変更しようとするとバグるのでとりあえず true
+				window.sandboxDeveloperProps.utils.fitToWindow(true);
 				resizeGrid();
 			},
 			revertSize: function(){
-				props.driver._platform.revertViewSize();
+				window.sandboxDeveloperProps.utils.revertViewSize();
 				resizeGrid();
 			},
 			captureScreen: 	function(e) {
