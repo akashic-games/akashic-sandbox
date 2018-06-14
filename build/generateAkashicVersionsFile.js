e6e4745fa694ac575c868e1136ed540984fda844
dera- <alchemy.and.fairy@gmail.com>
2018-06-14 16:19:24 +0900

    add versions.json under engine-src

--- build/generateAkashicVersionsFile.js
+++ build/generateAkashicVersionsFile.js
@@ -1,20 +1,37 @@
-var version = "v2";
-if (typeof process.argv[2] !== "undefined") {
-	switch (process.argv[2]) {
-		case "v1":
-		case "v2":
-			version = process.argv[2];
-			break;
-		default:
-			console.log("please designate version(v1 or v2)");
-			console.log("ex: node copyfiles.js v2");
-			throw new Error("invalid parameter");
-	}
-}
 var path = require("path");
 var fs = require("fs");
 
+if (process.argv.length < 4) {
+	console.log("Please enter command as follows: node copyfiles.js version outputPath");
+	console.log("ex: node copyfiles.js v2 engine-src/v2/");
+	throw new Error("invalid parameter");
+}
+
+// バージョン取得
+var version;
+switch (process.argv[2]) {
+	case "v1":
+	case "v2":
+		version = process.argv[2];
+		break;
+	default:
+		console.log("please designate version(v1 or v2)");
+		throw new Error("invalid parameter");
+}
+// 出力ファイルパス取得
+var outputPath = path.join(__dirname, "..", process.argv[3]);
+// ベースとするバージョン情報の取得
 var versions = {};
+if (typeof process.argv[4] !== "undefined") {
+	var baseFilePath = path.join(__dirname, "..", process.argv[4]);
+	fs.statSync(baseFilePath);
+	if (path.extname(baseFilePath) !== ".json") {
+		console.log("please specify json-file");
+		throw new Error("invalid parameter");
+	}
+	versions = JSON.parse(fs.readFileSync(path.join(__dirname, "..", process.argv[4]), "utf8"));
+}
+
 var akashicLibDir = path.resolve(__dirname + "/../engine-src/" + version + "/node_modules/@akashic");
 var akashicLibs = fs.readdirSync(akashicLibDir);
 console.log("generate versions file about " + version);
@@ -26,5 +43,5 @@ akashicLibs.forEach(function(libName) {
 	console.log(keyName + ":" + packageJson["version"]);
 });
 
-fs.writeFileSync(path.resolve(__dirname + "/../js/" + version + "/versions.json"), JSON.stringify(versions));
+fs.writeFileSync(path.join(outputPath, "versions.json"), JSON.stringify(versions));
 console.log("saved vesion of each akashic-library");
