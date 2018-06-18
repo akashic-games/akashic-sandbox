1078e288670fc5bb3b1eb0a5eb9e766386a23dee
dera- <alchemy.and.fairy@gmail.com>
2018-06-18 21:10:37 +0900

    reflect comment

--- build/checkAkashicVersions.js
+++ build/checkAkashicVersions.js
@@ -5,17 +5,14 @@ var fs = require("fs");
 	var originalPackageJson = require(path.join(__dirname, "..", "engine-src", version, "package.json"));
 	var versions = {};
 	["dependencies", "devDependencies", "optionalDependencies"].forEach(function(item) {
-		if (originalPackageJson[item] === undefined) {
-			return;
-		}
-		for (var libName in originalPackageJson[item]) {
+		for (var libName in (originalPackageJson[item] || {})) {
 			versions[libName] = originalPackageJson[item][libName];
 		}
 	});
 	var copiedVersionsJson = require(path.join(__dirname, "..", "engine-src", version, "external", "versions.json"));
 	for (var libName in copiedVersionsJson) {
 		if (copiedVersionsJson[libName] !== versions[libName]) {
-			console.log(`Expected ${libName}@${versions[libName]} to be v${copiedVersionsJson[libName]}`);
+			console.error(`Expected ${libName}@${copiedVersionsJson[libName]} to be v${versions[libName]}`);
 			process.exit(1);
 		} else {
 			console.log(`OK: ${libName}@${versions[libName]}`);
--- build/generateAkashicVersionsFile.js
+++ build/generateAkashicVersionsFile.js
@@ -2,43 +2,40 @@ var path = require("path");
 var fs = require("fs");
 
 if (process.argv.length < 3) {
-	console.log("Please enter command as follows: node generateAkashicVersionsFile.js version");
-	console.log("ex: node generateAkashicVersionsFile.js v2");
-	throw new Error("invalid parameter");
+	console.error("Please enter command as follows: node generateAkashicVersionsFile.js version");
+	console.error("ex: node generateAkashicVersionsFile.js v2");
+	process.exit(1);
 }
 
 // バージョン取得
 var version = process.argv[2];
 if (! /^v[12]$/.test(version)) {
-	console.log("please specify version(v1 or v2)");
-	throw new Error("invalid parameter");
+	console.error("please specify version(v1 or v2)");
+	process.exit(1);
 }
 
 console.log(`generate versions file about ${version}`);
-console.log("get vesion of each akashic-library");
+console.log("get version of each akashic-library");
 var originalPackageJson = require(path.join(__dirname, "..", "engine-src", version, "package.json"));
 var libVersions = {};
 ["dependencies", "devDependencies", "optionalDependencies"].forEach(function(item) {
-	if (originalPackageJson[item] === undefined) {
-		return;
-	}
-	for (var libName in originalPackageJson[item]) {
+	for (var libName in (originalPackageJson[item] || {})) {
 		libVersions[libName] = originalPackageJson[item][libName];
 	}
 });
-var akashicLibDir = path.resolve(__dirname + "/../engine-src/" + version + "/node_modules/@akashic");
+var akashicLibDir = path.join(__dirname, "..", "engine-src", version, "node_modules", "@akashic");
 var akashicLibs = ["akashic-engine", "game-driver", "game-storage", "pdi-browser"];
 var versions = {};
 akashicLibs.forEach(function(libName) {
 	var packageJson = require(path.join(akashicLibDir, libName, "package.json"));
 	var keyName = "@akashic/" + libName;
 	if (libVersions[keyName] !== packageJson["version"]) {
-		console.log(`Expected ${keyName}@${libVersions[keyName]} to be v${packageJson["version"]}`);
-		throw new Error("current vesion is not expected");
+		console.error(`Expected ${keyName}@${packageJson["version"]} to be v${libVersions[keyName]}`);
+		process.exit(1);
 	}
 	versions[keyName] = packageJson["version"];
 	console.log(`${keyName}:${packageJson["version"]}`);
 });
 
-fs.writeFileSync(path.join(__dirname, "..", "engine-src", version, "external", "versions.json"), JSON.stringify(versions));
-console.log("saved vesion of each akashic-library");
+fs.writeFileSync(path.join(__dirname, "..", "engine-src", version, "external", "versions.json"), JSON.stringify(versions, null, 2));
+console.log("saved version of each akashic-library");
