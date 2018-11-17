d181cfa37a7bfdac13ac4a815aed444f10731559
dera- <alchemy.and.fairy@gmail.com>
2018-11-17 20:14:04 +0900

    reflect comment

--- engine-src/common/js/warning-es6.js
+++ engine-src/common/js/warning-es6.js
@@ -1,6 +1,4 @@
 function warningEs6OnConsole() {
-	console.debug("ES5でサポートされないオブジェクト・メソッドがコンテンツから呼ばれる毎に警告を出します。");
-	console.debug("警告を非表示にする場合は、右上の歯車をクリックして「ES5でサポートされない機能が使用された場合警告を出力(リロードで適用)」にチェックを入れてリロードしてください。");
 	var objectNames = ["Map", "Set", "WeakMap", "WeakSet", "Promise", "Symbol", "SharedArrayBuffer", "Atomics", "Proxy"];
 	var staticMethodNames = {
 		"Array": ["from", "of"],
@@ -15,48 +13,61 @@ function warningEs6OnConsole() {
 		"Array": ["fill", "find", "entries", "keys", "values", "copyWithin", "includes"],
 		"String": ["codePointAt", "startsWith", "endsWith", "includes", "repeat", "normalize", "padEnd", "padStart"]
 	};
+	var noWarning = true;
 	if (window.Proxy && window.Reflect) {
 		objectNames.forEach(function (name) {
 			if (!window[name]) {
-				console.debug(name + "はブラウザでサポートされていない機能です。");
 				return;
 			}
 			window[name] = new Proxy(window[name], {
 				construct: function (target, args) {
-					console.warn(name + "が実行されました。akashic-engineでは" + name + "の動作が保証されていないので、使用しないことを推奨します。");
+					if (noWarning) {
+						displayFirstWarning();
+						noWarning = false;
+					}
+					console.warn(name + "が使用されました。Akashic Engineでは" + name + "の動作が保証されていないので、使用しないことを推奨します。");
 					return Reflect.construct(target, args);
 				}
 			});
 		});
-	} else {
-		console.debug("Proxyがブラウザでサポートされていないため、ES5でサポートされないオブジェクトの検知はできません。検知を可能にするためには、Proxyをサポートしているブラウザを利用してください。");
 	}
 	Object.keys(staticMethodNames).forEach(function(name) {
 		staticMethodNames[name].forEach(function(methodName) {
-			var method = name + "." + methodName;
 			if (!window[name] || !window[name][methodName]) {
-				console.debug(method + "はブラウザでサポートされていない機能です。");
 				return;
 			}
 			var original = window[name][methodName];
 			window[name][methodName] = function() {
-				console.warn(method + "が実行されました。akashic-engineでは" + method + "の動作が保証されていないので、使用しないことを推奨します。");
+				if (noWarning) {
+					displayFirstWarning();
+					noWarning = false;
+				}
+				var method = name + "." + methodName;
+				console.warn(method + "が実行されました。Akashic Engineでは" + method + "の動作が保証されていないので、使用しないことを推奨します。");
 				return original.apply(this, arguments);
 			};
 		});
 	});
 	Object.keys(methodNames).forEach(function(name) {
 		methodNames[name].forEach(function(methodName) {
-			var method = name + ".prototype." + methodName;
 			if (!window[name] || !window[name].prototype[methodName]) {
-				console.debug(method + "はブラウザでサポートされていない機能です。");
 				return;
 			}
 			var original = window[name].prototype[methodName];
 			window[name].prototype[methodName] = function() {
-				console.warn(method + "が実行されました。akashic-engineでは" + method + "の動作が保証されていないので、使用しないことを推奨します。");
+				if (noWarning) {
+					displayFirstWarning();
+					noWarning = false;
+				}
+				var method = name + ".prototype." + methodName;
+				console.warn(method + "が実行されました。Akashic Engineでは" + method + "の動作が保証されていないので、使用しないことを推奨します。");
 				return original.apply(this, arguments);
 			};
 		});
 	});
+}
+
+function displayFirstWarning() {
+	console.debug("ES5でサポートされないオブジェクト・メソッドがコンテンツから呼ばれる毎に警告を出します。");
+	console.debug("警告を非表示にする場合は、右上の歯車をクリックして「ES5でサポートされない機能が使用された場合警告を出力(リロードで適用)」にチェックを入れてリロードしてください。");
 }
