34ca0afa4dcbde1d172fb9d076249c22c8c31fc4
dera- <alchemy.and.fairy@gmail.com>
2019-02-19 21:09:16 +0900

    reflect comment

--- build/generateFilesAboutEngineFiles.js
+++ build/generateFilesAboutEngineFiles.js
@@ -15,15 +15,15 @@ var versions = {
 fs.writeFileSync(path.join(__dirname, "..", "engine-src", "engineFilesVersion.json"), JSON.stringify(versions, null, 2));
 console.log("end to generate engineFilesVersion.json");
 
-var promises = Object.keys(versions).map(version => {
-	var fileName = `engineFilesV${versions[version].replace(/\./g, "_")}.js`;
+var promises = Object.keys(versions).map(majorVersion => {
+	var fileName = `engineFilesV${versions[majorVersion].replace(/\./g, "_")}.js`;
 	return Promise.resolve().then(() => {
-		console.log(`start to download engineFiles (v${versions[version]})`);
-		return fetch(`https://github.com/akashic-games/engine-files/releases/download/v${versions[version]}/${fileName}`);
+		console.log(`start to download engineFiles (v${versions[majorVersion]})`);
+		return fetch(`https://github.com/akashic-games/engine-files/releases/download/v${versions[majorVersion]}/${fileName}`);
 	}).then(res => {
 		return new Promise((resolve, reject) => {
-			var fileName = `engineFilesV${versions[version].replace(/\./g, "_")}.js`;
-			var fileStream = fs.createWriteStream(path.join(__dirname, "..", "engine-src", version, "external", fileName));
+			var fileName = `engineFilesV${versions[majorVersion].replace(/\./g, "_")}.js`;
+			var fileStream = fs.createWriteStream(path.join(__dirname, "..", "engine-src", majorVersion, "external", fileName));
 			res.body.pipe(fileStream);
 			res.body.on("error", (err) => {
 				reject(err);
@@ -32,7 +32,7 @@ var promises = Object.keys(versions).map(version => {
 				resolve();
 			});
 		});
-	}).then(() => {console.log(`end to download engineFiles (v${versions[version]})`);});
+	}).then(() => {console.log(`end to download engineFiles (v${versions[majorVersion]})`);});
 });
 Promise.all(promises)
 	.then(() => {console.log("end to generate files");})
