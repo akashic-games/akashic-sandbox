bd514167a9e3d50114e2728970a7f37aa9bfc4bc
kamakiri01 <kamakiri01@users.noreply.github.com>
2019-08-27 17:59:09 +0900

    fix global vars

--- src/client/akashic/meddling/MeddlingDate.ts
+++ src/client/akashic/meddling/MeddlingDate.ts
@@ -1,13 +1,24 @@
-var MeddlingDate = new Proxy(Date, {
-	get: (target, prop, receiver) => {
-		if (prop === "now") {
-			console.warn("Date.now()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。");
-			window.dispatchEvent(new ErrorEvent("akashicWarning", {
-				message: "Date.now()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。"
-			}));
-		}
-		return (target as any)[prop];
+
+var MeddlingDate;
+
+(function () {
+	// ieではProxy未対応なので差し替えない
+	var agent = window.navigator.userAgent;
+	if ((agent.indexOf('msie 9.') !== -1) || (agent.indexOf('trident/7') !== -1)) {
+		MeddlingDate = Date;
+	} else {
+		MeddlingDate = new Proxy(Date, {
+			get: (target, prop, receiver) => {
+				if (prop === "now") {
+					console.warn("Date.now()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。");
+					window.dispatchEvent(new ErrorEvent("akashicWarning", {
+						message: "Date.now()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。"
+					}));
+				}
+				return (target as any)[prop];
+			}
+		});
 	}
-});
+})();
 
 (window as any).MeddlingDate = MeddlingDate;
--- src/client/akashic/meddling/MeddlingMath.ts
+++ src/client/akashic/meddling/MeddlingMath.ts
@@ -1,13 +1,18 @@
-var MeddlingMath = new Proxy(Math, {
-	get: (target, prop, receiver) => {
-		if (prop === "random") {
-			console.warn("Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。");
-			window.dispatchEvent(new ErrorEvent("akashicWarning", {
-				message: "Date.now()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。"
-			}));
+var MeddlingMath
+
+
+(function () {
+	MeddlingMath = new Proxy(Math, {
+		get: (target, prop, receiver) => {
+			if (prop === "random") {
+				console.warn("Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。");
+				window.dispatchEvent(new ErrorEvent("akashicWarning", {
+					message: "Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。"
+				}));
+			}
+			return (target as any)[prop];
 		}
-		return (target as any)[prop];
-	}
-});
+	});	
+})();
 
 (window as any).MeddlingMath = MeddlingMath;
