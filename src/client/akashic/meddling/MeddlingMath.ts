92a20ac0425453cd72bd2a10f5023cea07d945e4
kamakiri01 <kamakiri01@users.noreply.github.com>
2019-08-29 11:58:01 +0900

    fix math

--- src/client/akashic/meddling/MeddlingMath.ts
+++ src/client/akashic/meddling/MeddlingMath.ts
@@ -1,17 +1,23 @@
 var MeddlingMath;
 
 (function (): void {
-	MeddlingMath = new Proxy(Math, {
-		get: (target, prop, receiver) => {
-			if (prop === "random") {
-				console.warn("Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。");
-				window.dispatchEvent(new ErrorEvent("akashicWarning", {
-					message: "Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。"
-				}));
+	// ieではProxy未対応なので差し替えない
+	var agent = window.navigator.userAgent;
+	if ((agent.indexOf("msie 9.") !== -1) || (agent.indexOf("trident/7") !== -1)) {
+		MeddlingMath = Math;
+	} else {
+		MeddlingMath = new Proxy(Math, {
+			get: (target, prop, receiver) => {
+				if (prop === "random") {
+					console.warn("Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。");
+					window.dispatchEvent(new ErrorEvent("akashicWarning", {
+						message: "Math.random()が実行されました。Akashicコンテンツではこの機能に依存してゲームの実行状態が変わらないようにしてください。"
+					}));
+				}
+				return (target as any)[prop];
 			}
-			return (target as any)[prop];
-		}
-	});
+		});
+	}
 })();
 
 (window as any).MeddlingMath = MeddlingMath;
