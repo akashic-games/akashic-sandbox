266d6fa9fe19ed1b8d39a073c5f8b01152ede9bc
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-06-08 14:50:40 +0900

    Fix engineFilesVariable

--- src/server/controller/game.ts
+++ src/server/controller/game.ts
@@ -1,20 +1,21 @@
 import express = require("express");
 
-var controller: express.RequestHandler = (req: express.Request, res: express.Response, _next: Function) => {
-	var devMode = req.query.devmode !== "disable";
-	var environment = res.locals.environment;
-	var version = environment && environment["sandbox-runtime"] ? environment["sandbox-runtime"] : "1";
+const controller: express.RequestHandler = (req: express.Request, res: express.Response, _next: Function) => {
+	const devMode = req.query.devmode !== "disable";
+	const environment = res.locals.environment;
+	const version = environment && environment["sandbox-runtime"] ? environment["sandbox-runtime"] : "1";
 	// json の読み込みのため require の lint エラーを抑止
 	/* eslint-disable @typescript-eslint/no-var-requires */
-	var versionsJson = require("../engineFilesVersion.json");
-	var pkgJson = require("../../package.json");
+	const pkgJson = require("../../package.json");
+	const engineFilesPkgJson = require(`../../node_modules/aev${version}/package.json`);
 	/* eslint-enable */
+	const engineFilesVariable = `engineFilesV${engineFilesPkgJson.version.replace(/[\.-]/g, "_") }`;
 
 	res.render("game", {
 		title: `akashic-sandbox v${pkgJson.version}`,
 		version: version,
 		devMode: devMode,
-		engineFilesVariable: versionsJson[`v${version}`].variable
+		engineFilesVariable: engineFilesVariable
 	});
 };
 
