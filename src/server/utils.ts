f1afd07225c4a0eb69b93c682836fa18b901bf83
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-06-09 10:44:16 +0900

    Mod resolveEngineFilesVariable

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,3 +1,5 @@
-export function resolveEngineFilesVariable(version: string): string {
+type EngineFilesVersion = "1" | "2" | "3";
+
+export function resolveEngineFilesVariable(version: EngineFilesVersion): string {
 	return `engineFilesV${version.replace(/[\.-]/g, "_")}`;
 }
